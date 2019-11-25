f1d8e06970486cad9c79b7b0d3469e459554aba2
lceron2010 <laura.ceron@epn.edu.ec>
2019-11-25 17:25:34 -0500

    componente ejemplo2

--- 03-Angular/componentes/src/app/mi-primer-componente/mi-primer-componente.component.ts
+++ 03-Angular/componentes/src/app/mi-primer-componente/mi-primer-componente.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-mi-primer-componente',
   templateUrl: './mi-primer-componente.component.html',
   styleUrls: ['./mi-primer-componente.component.scss']
 })
-export class MiPrimerComponenteComponent implements OnInit {
+export class MiPrimerComponenteComponent implements OnInit , OnDestroy{
 
-  constructor() { }
+  @Input()
+  public  titulo:string
 
-  ngOnInit() {
-    salular()
-    {
-      alert('HOLAAAAAA');
-    }
+  @Input()
+  public imagen:string;
+
+  @Input()
+  public textoImagen:string;
+
+  @Input()
+  public textoBoton:string;
+
+  constructor() {
+    console.log('instanciando'); //no se debe meter logica de negocio en el constructor
+  } //instancia la clase(ayuda ->angular)
+
+  ngOnInit() { //la clase esta lista, hace algo al inicio
+    console.log('esta listo');
+    console.log(this.titulo);
+    console.log(this.textoImagen);
+    console.log(this.textoBoton);
   }
 
+  ngOnDestroy(): void { //la clase esta por destruirse, //el void regresa null no es necesario escribir
+
+  }
+
+  saludar()
+  {
+    alert('HOLAAAAAA');
+  }
+
+
+//class (etiquta html)
+//  instancia a la calse
+//  clase esta lista
+// clase se destruye
 }
