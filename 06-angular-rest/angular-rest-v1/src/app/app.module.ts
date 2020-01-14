615ded26246987003f9c3859a9a46cb3b5bbf461
lceron2010 <laura.ceron@epn.edu.ec>
2020-01-14 10:34:17 -0500

    filter table(primeng) - instalar angular material

--- 06-angular-rest/angular-rest-v1/src/app/app.module.ts
+++ 06-angular-rest/angular-rest-v1/src/app/app.module.ts
@@ -6,6 +6,10 @@ import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {UsuarioRestService} from "./services/rest/usuario-rest.service";
 import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import {InputTextModule} from "primeng";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from "@angular/material/input";
 @NgModule({
   declarations: [
     AppComponent //componentes
@@ -15,7 +19,11 @@ import {ButtonModule} from 'primeng/button';
     AppRoutingModule,
     FormsModule, //formularios
     HttpClientModule, //http
-    ButtonModule
+    ButtonModule,
+    TableModule,
+    InputTextModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [ //servicios
     UsuarioRestService
